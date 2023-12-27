e86348ce6f4324318710eeacd42e37c3c576a949
Adrit-3001 <adrit.panday@mail.utoronto.ca>
2023-12-26 19:53:47 -0500

    first commit to check things

---
+++ app/src/main/java/com/example/myapplication/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Your initialization code goes here
+    }
+}
+
